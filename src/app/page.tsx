3d553573f92bdafd5de06ee0d2d7ea02ad3950cd
Archit Kakkar <106911307+architkakkar@users.noreply.github.com>
2024-08-11 12:43:09 +0000

    Initial Commit.

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="bg-black text-white h-screen w-full text-4xl flex justify-center items-center">
+        Hello World!
+      </h1>
+    </>
+  );
+}
