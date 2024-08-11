330340ab6ea6602a071ef953b938ba73a85d0643
Archit Kakkar <106911307+architkakkar@users.noreply.github.com>
2024-08-11 14:03:08 +0000

    styled active link in navbar.

---
+++ src/app/issues/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function IssuesPage() {
+  return (
+    <div>
+      <h1>Issues</h1>
+      {/* Add content here */}
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
-import Link from "next/link";
+"use client";
 
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 
 export default function Navbar() {
 	const navLinks = [
 		{ label: "Dashboard", href: "/" },
 		{ label: "Issues", href: "/issues" },
 	];
+
+	const currentPath = usePathname();
 
 	return (
 		<nav className="flex space-x-10 border-b mb-5 px-5 py-5 items-center">
@@ -18,7 +24,11 @@ export default function Navbar() {
 					<li key={link.label}>
 						<Link
 							href={link.href}
-							className="text-zinc-500 hover:text-zinc-900 transition-colors"
+							className={classnames({
+								"text-zinc-950 hover:no-underline": link.href === currentPath,
+								"text-zinc-500 hover:underline": link.href !== currentPath,
+								"transition-colors font-medium": true,
+							})}
 						>
 							{link.label}
 						</Link>
